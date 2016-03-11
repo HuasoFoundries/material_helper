8d9384f4ee3cd33744bee78fa3229ede84eb1bed
Amenadiel <amenadiel@gmail.com>
2016-03-11 11:37:00 -0300

    Tag v0.3.0

--- config.js
+++ config.js
@@ -20,16 +20,16 @@ System.config({
 
   map: {
     "hammerjs": "npm:hammerjs@2.0.6",
-    "jquery": "github:instagis/jquery_helper@0.5.2",
+    "jquery": "github:instagis/jquery_helper@0.5.5",
     "underscore": "npm:lodash@3.10.1",
     "velocity": "github:julianshapiro/velocity@1.2.3",
-    "github:instagis/jquery_helper@0.5.2": {
+    "github:instagis/jquery_helper@0.5.5": {
       "css": "github:systemjs/plugin-css@0.1.20",
       "jquery": "npm:jquery@2.2.1",
       "jquery-csv": "github:evanplaice/jquery-csv@0.8.1",
       "jquery-serializejson": "npm:jquery-serializejson@2.6.2",
       "jquery.cookie": "github:carhartl/jquery-cookie@1.4.1",
-      "jquery.waitforChild": "npm:jquery.waitforChild@0.1.1",
+      "jquery.waitforChild": "npm:jquery.waitforChild@1.0.1",
       "lodash": "github:lodash/lodash@3.10.1"
     },
     "github:jspm/nodelibs-assert@0.1.0": {
